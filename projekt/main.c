ec899c3bed96add0be4239c25954808054bdbf9b
Barteq3012 <bartek.cz3012@gmail.com>
2020-04-04 18:41:21 +0200

    Update main.c

--- projekt/main.c
+++ projekt/main.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+
+int x_max = 80;
+int y_max = 40;
+int final_scale = 100;
+char* grass_array = NULL;
+int* water_array = NULL;
+
+short* final_small_array = NULL;
+short* final_array = NULL;
+
+void grass_load(FILE* fp)
+{
+   grass_array = calloc(x_max * y_max, sizeof(char));
+   water_array = calloc(x_max * y_max, sizeof(int));
+
+   final_small_array = calloc(x_max * y_max, sizeof(short));
+   final_array = calloc(x_max * final_scale * y_max * final_scale, sizeof(short));
+
+   int x = 0;
+   int y = 0;
+
+   char ch;
+   while ((ch = (char) getc(fp)) != EOF)
+   {
+      if(ch == '*' || ch == '-')
+      {
+         grass_array[y * x_max + x] = ch;
+      }
+      else
+      {
+         continue; //tu mozna zrobic obsluge bledu
+      }
+      
+      x++;
+      if(x == x_max)
+      {
+         x = 0;
+         y++;
+      }
+   }
 }
-//kom2
