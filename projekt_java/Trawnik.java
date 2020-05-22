84003651a3bd5dfddd3b9a4501026c6c565c8fad
luizalouise <lkluiza12@gmail.com>
2020-05-22 08:05:57 +0200

    read from file

---
+++ projekt_java/Trawnik.java
@@ -0,0 +1,28 @@
+package trawnik;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Trawnik {
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<Character>grass_array= new ArrayList<>();
+        File f = new File(args[0]);
+        FileReader fr=new FileReader(f);
+        BufferedReader br=new BufferedReader(fr);
+        int c=0;
+        char ch;        
+                while((c=br.read())!=-1){
+                    ch = (char) c;
+ 		if(ch=='*' || ch=='-')
+                    grass_array.add(ch);
+                }
+      
+          System.out.println(grass_array.size());
+    }
+        
+    }
