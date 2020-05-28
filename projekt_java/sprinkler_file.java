e8205283d42ad6a0851d48b4090ba85109a958a8
luizalouise <lkluiza12@gmail.com>
2020-05-28 21:17:23 +0200

    add sprinkler_file

---
+++ projekt_java/sprinkler_file.java
@@ -0,0 +1,23 @@
+package trawnik;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+
+public class sprinkler_file extends all_in {
+        
+    
+   public void writePositionsToFile(String name) throws FileNotFoundException {
+        PrintWriter file = new PrintWriter(name);
+        file.println("Position x:  Position y:  Type:  Rotation:");
+        for (int i = 0; i < water_array.size(); i++) {
+            file.println("   " + water_array.get(i).getX() + "          " + water_array.get(i).getY() + "        " + water_array.get(i).getType() + "          " + water_array.get(i).getRotation());
+        }
+        file.close();
+    }
+
+
+   
+
+}
